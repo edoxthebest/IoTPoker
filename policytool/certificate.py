3a05bb09815f1df5971d2e42b27069c7f28fcd73
Edoardo Lunati <edo.luna98@hotmail.it>
2024-05-26 14:01:16 +0200

    Refactore Certificate - need testing

--- policytool/certificate.py
+++ policytool/certificate.py
@@ -1,13 +1,14 @@
 from policytool.iot_policy import IoTPolicy
 
 class Certificate:
-  count = 0
+  @property
+  def name(self):
+    return self._name
   
-  def __init__(self, name, policies: IoTPolicy):
-    self.name = name
-    self.policies = policies
-
-  def __init__(self, policy: IoTPolicy):
-    self.name = f'cert{Certificate.count}'
-    self.policies = policy
-    Certificate.count += 1
+  @property
+  def policy(self):
+    return self._policy
+  
+  def __init__(self, policies: list[IoTPolicy], name: str = None):
+    self._policy = IoTPolicy.get_union(policies)
+    self._name = name if not name is None else self.policy.get_client()
---
+++ tests/test_certificate.py
@@ -0,0 +1,4 @@
+import unittest
+
+class TestCertificate(unittest.TestCase):
+  pass
