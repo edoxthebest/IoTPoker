1d67059291b3f587c74c30c78864f9856a41def8
Edoardo Lunati <edo.luna98@hotmail.it>
2024-10-21 22:55:49 +0200

    Improved case study demo + fixed version

--- case_study_demo.py
+++ case_study_demo.py
@@ -1,14 +1,25 @@
+import argparse
 import json
+import logging
 import time
 from policytool import Certificate, PolicyGraph, PolicyReader, Prover, Thing
 
+parser = argparse.ArgumentParser(description='Evaluates the case study.')
+parser.add_argument('--fixed', action='store_true',
+                    help='Uses the corrected policy version')
+parser.add_argument('--debug', action='store_true')
+args = parser.parse_args()
+if args.debug:
+  logging.getLogger('IoT:Poker').setLevel(logging.DEBUG)
+
 case_study_dir = 'tests/policies/case-study/'
 things_dir = 'tests/things/'
+cfg_filename = 'case_study_fixed.config' if args.fixed else 'case_study.config'
 
 things = []
 certs = []
 start_time = time.time()
-with open('case_study.config') as config_file:
+with open(cfg_filename) as config_file:
   config = json.load(config_file)
   for thing_json in config['things']:
     thing_pol = PolicyReader.read_policy_file(case_study_dir + thing_json['policy'])
@@ -18,14 +29,14 @@ with open('case_study.config') as config_file:
     cert_pol = PolicyReader.read_policy_file(case_study_dir + cert_json['policy'])
     cert = Certificate([cert_pol], cert_json['name'])
     certs.append(cert)
-Prover.print_time(start_time)
-print(f'Read {len(certs)} policy certificates and {len(things)} things.')
+print(f'[{time.time() - start_time:.4f}] '
+      f'Read {len(certs)} policy certificates and {len(things)} things.')
 
 start_time = time.time()
 policy_graph = PolicyGraph(certs + things)
 policy_graph.build_sym_graph()
-Prover.print_time(start_time)
-print(f'Built symbolic information flow graph of {policy_graph.size} nodes.')
+print(f'[{time.time() - start_time:.4f}] '
+      f'Built symbolic information flow graph of {policy_graph.size} nodes.')
 
 # policy_graph.draw_tree(['floor1_badge_reader'])
 
@@ -34,7 +45,10 @@ prover.reach('floor1_door_lock', 'floor2_light', log_level='info')
 prover.reach('elevator', 'floor1_door_lock', log_level='info')
 
 prover.reach_only('elevator', [], log_level='info')
-prover.reach_only('floor1_door_lock', ['floor1_light', 'presenceSensor1', 'lambda_logger'], log_level='info')
+if args.fixed:
+  prover.reach_only('floor1_door_lock', ['light1', 'presenceSensor1', 'lambda_logger'], log_level='info')
+else:
+  prover.reach_only('floor1_door_lock', ['floor1_light', 'presenceSensor1', 'lambda_logger'], log_level='info')
 prover.reach_only('floor1_smoke_sensor', ['elevator', 'floor1_door_lock'], log_level='info')
 prover.reach_only('elevator', ['floor2_water_pump'], log_level='info')
 
@@ -53,10 +67,17 @@ prover.only_reached_by('floor1_fire_siren', [
     'lambda_fire_alarm',
     'elevator'
     ], log_level='info')
-prover.only_reached_by('floor1_light', ['lambda_fire_alarm'], log_level='info')
+if args.fixed:
+  prover.only_reached_by('light1', ['lambda_fire_alarm'], log_level='info')
+else:
+  prover.only_reached_by('floor1_light', ['lambda_fire_alarm'], log_level='info')
 
 prover.isolated(['floor1_badge_reader'], ['floor1_water_pump', 'elevator'], log_level='info')
 prover.isolated(['floor2_badge_reader'],
                 ['lambda_fire_alarm', 'elevator', 'floor2_smoke_sensor'], log_level='info')
-prover.isolated(['floor1_light', 'floor2_light'],
+if args.fixed:
+    prover.isolated(['light1', 'light2'],
+                ['lambda_fire_alarm', 'elevator', 'floor2_smoke_sensor'], log_level='info')
+else:
+  prover.isolated(['floor1_light', 'floor2_light'],
                 ['lambda_fire_alarm', 'elevator', 'floor2_smoke_sensor'], log_level='info')
